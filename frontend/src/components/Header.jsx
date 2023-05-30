bd8394135cad8e4425eccbd8bb19b41d46ab3e2e
blessja <jaydenj772@gmail.com>
2023-05-30 03:13:32 +0200

    mobile queries added

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,61 +1,81 @@
-import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa'
-import { Link, useNavigate } from 'react-router-dom'
-import { useSelector, useDispatch } from 'react-redux'
-import { logout, reset } from '../features/auth/authSlice'
-import { logoutStaff, resetStaff } from '../features/staff/staffSlice'
-
+import { FaBars, FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout, reset } from '../features/auth/authSlice';
+import { logoutStaff, resetStaff } from '../features/staff/staffSlice';
 
 function Header() {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-  const { user } = useSelector((state) => state.auth)
-  const { staff } = useSelector((state) => state.staff)
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+  const { staff } = useSelector((state) => state.staff);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const closeMobileMenu = () => {
+    setIsMobileMenuOpen(false);
+  };
 
   const onLogout = () => {
-    dispatch(logout())
-    dispatch(logoutStaff())
-    dispatch(reset())
-    dispatch(resetStaff())
-    navigate('/')
-  }
+    dispatch(logout());
+    dispatch(logoutStaff());
+    dispatch(reset());
+    dispatch(resetStaff());
+    navigate('/');
+  };
 
   return (
-    <header style={{background: 'white', padding: '10px', display: 'flex', justifyContent: 'space-between', alignItems: 'center'}} className='header'>
-      <div style={{display: 'flex', alignItems: 'center', gap: '20px'}} className='logo'>
-        <img style={{width: '100px', height: '100px'}} src={require('../images/WhatsApp Image 2023-05-17 at 12.22.35.jpg')} alt="" />
-        <Link className='logo-text' to='/' style={{fontSize: '1.8rem', fontWeight: 'bold', color: 'black', textDecoration: 'none'}}>Orange Car Wash </Link>
+    <header
+      style={{ background: 'white', padding: '10px', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}
+      className='header'
+    >
+      <div style={{ display: 'flex', alignItems: 'center', gap: '20px' }} className='logo'>
+        <img style={{ width: '100px', height: '100px' }} src={require('../images/WhatsApp Image 2023-05-17 at 12.22.35.jpg')} alt='' />
+        <Link className='logo-text' to='/' style={{ fontSize: '1.8rem', fontWeight: 'bold', color: 'black', textDecoration: 'none' }}>
+          Orange Car Wash
+        </Link>
       </div>
-      <ul>
-        {user !== staff ? (
-          <li>
-            <button className='btn' onClick={onLogout}>
-              <FaSignOutAlt /> Logout
-            </button>
-          
-          </li>
-        ) : (
-          <>
-            <li>
-              <Link to='/staff/login'>
-                <FaSignInAlt />
-                Staff Login
-              </Link>
-            </li>
-            <li>
-              <Link to='/login'>
-                <FaSignInAlt /> Login Customer
-              </Link>
-            </li>
-            <li>
-              <Link to='/register'>
-                <FaUser /> Register Customer
-              </Link>
-            </li>
-          </>
+
+      {/* Hamburger menu */}
+      <div className='mobile-menu'>
+        <FaBars onClick={toggleMobileMenu} />
+        {isMobileMenuOpen && (
+          <ul className='mobile-menu-items'>
+            {user !== staff ? (
+              <li onClick={closeMobileMenu}>
+                <button className='btn' onClick={onLogout}>
+                  <FaSignOutAlt /> Logout
+                </button>
+              </li>
+            ) : (
+              <>
+                <li onClick={closeMobileMenu}>
+                  <Link to='/staff/login'>
+                    <FaSignInAlt />
+                    Staff Login
+                  </Link>
+                </li>
+                <li onClick={closeMobileMenu}>
+                  <Link to='/login'>
+                    <FaSignInAlt /> Login Customer
+                  </Link>
+                </li>
+                <li onClick={closeMobileMenu}>
+                  <Link to='/register'>
+                    <FaUser /> Register Customer
+                  </Link>
+                </li>
+              </>
+            )}
+          </ul>
         )}
-      </ul>
+      </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
