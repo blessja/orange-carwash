212829d203b4a641f897c12778e050476b7d6560
blessing <jaydenj772@gmail.com>
2023-06-27 15:19:05 +0200

    the url changed

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb+srv://admin-Jackson:jaydenjackson1@cluster0.bnu3c.mongodb.net/orange-carwash?retryWrites=true&w=majority')
+    const conn = await mongoose.connect(`${process.env.MONGODB_URI}`)
 
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
   } catch (error) {
