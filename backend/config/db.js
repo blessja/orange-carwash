ba02ed26fac33fb10bf8739328a9bc10f4704b67
blessing <jaydenj772@gmail.com>
2023-06-30 21:00:48 +0200

    link updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(`${process.env.MONGODB_URI}`)
+    const conn = await mongoose.connect(`mongodb+srv://admin-Jackson:jaydenjackson1@cluster0.bnu3c.mongodb.net/orange-carwash-2?retryWrites=true&w=majority`)
 
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
   } catch (error) {
